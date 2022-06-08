e70fe1ddb49128f08d986e11fe472d6a05d8dd2e
Hyo Gyeong <cdkrcd8@gmail.com>
2022-06-08 11:26:25 +0900

    implement: customer jpa

--- src/main/java/com/example/oisan/domain/OiPayUsage.java
+++ src/main/java/com/example/oisan/domain/OiPayUsage.java
@@ -4,24 +4,41 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="OIPAYUSAGE")
+@SequenceGenerator(
+        name = "OI_PAY_SEQ_GENERATOR",
+        sequenceName = "OI_PAY_SEQ", // 시퀸스 명
+        initialValue = 1, // 초기 값
+        allocationSize = 1 // 미리 할당 받을 시퀸스 수
+)
 public class OiPayUsage {
-	@Column
+	@Column(name="customer_id")
 	private int customerId;
+	
 	@Id
+	@Column(name="oi_pay_id")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="OI_PAY_SEQ_GENERATOR")
 	private int oiPayId;
+	
 	@Column
 	private int type;
+	
 	@Column
 	private int amount;
+	
 	@Column
 	private int remain;
-	@Column(name="action_id")
-	private int auctionId;
+	
+	@Column(name="auction_id")
+	private Integer auctionId;
+	
 	@Column(name="create_at")
 	private Date createAt;
 	
@@ -31,6 +48,16 @@ public class OiPayUsage {
 		super();
 		this.customerId = customerId;
 		this.oiPayId = oiPayId;
+		this.type = type;
+		this.amount = amount;
+		this.remain = remain;
+		this.auctionId = auctionId;
+		this.createAt = createAt;
+	}
+	
+	public OiPayUsage(int customerId, int type, int amount, int remain, Integer auctionId, Date createAt) {
+		super();
+		this.customerId = customerId;
 		this.type = type;
 		this.amount = amount;
 		this.remain = remain;
