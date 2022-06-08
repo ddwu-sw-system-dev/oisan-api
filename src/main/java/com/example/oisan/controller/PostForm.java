3f74e9b5c9429da8176825e729406648bbab668c
HyeJun <solar3070@naver.com>
2022-06-08 18:52:49 +0900

    Feat:  PostCommand  추가

---
+++ src/main/java/com/example/oisan/controller/PostForm.java
@@ -0,0 +1,81 @@
+package com.example.oisan.controller;
+
+import javax.persistence.Column;
+import java.util.Date;
+import java.util.List;
+
+public class PostForm {
+
+    private String title;
+    private String desc;
+    private String imageUrl;
+    private int width;
+    private int height;
+    private int depth;
+    private int categId;
+    private List<String> tagList;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public int getCategId() {
+        return categId;
+    }
+
+    public void setCategId(int categId) {
+        this.categId = categId;
+    }
+
+    public List<String> getTagList() {
+        return tagList;
+    }
+
+    public void setTagList(List<String> tagList) {
+        this.tagList = tagList;
+    }
+}
