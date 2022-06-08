99ebf877b3a72367bfa392a434df23ec48a43161
HyeJun <solar3070@naver.com>
2022-06-08 18:51:08 +0900

    Feat: ã PostRepository 추가

---
+++ src/main/java/com/example/oisan/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package com.example.oisan.repository;
+
+import com.example.oisan.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    Optional<Post> findByTitle(String title);
+
+
+}
