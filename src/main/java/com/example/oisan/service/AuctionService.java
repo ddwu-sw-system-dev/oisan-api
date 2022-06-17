3d72d24331f235df902f315846fdfa0668af5936
janu01ary <64868883+janu01ary@users.noreply.github.com>
2022-06-18 00:23:08 +0900

    Update AuctionService.java

--- src/main/java/com/example/oisan/service/AuctionService.java
+++ src/main/java/com/example/oisan/service/AuctionService.java
@@ -3,7 +3,6 @@ package com.example.oisan.service;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
