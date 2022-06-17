8084793493548fb71ee66f9f9f26dd69ddbc91e2
HyeJun <solar3070@naver.com>
2022-06-17 18:10:51 +0900

    feat: 추가 함수, 수정 함수 분리

--- src/main/java/com/example/oisan/service/PostService.java
+++ src/main/java/com/example/oisan/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.oisan.service;
 import com.example.oisan.controller.PostCommand;
 import com.example.oisan.domain.Customer;
 import com.example.oisan.domain.Post;
+import com.example.oisan.repository.CustomerRepository;
 import com.example.oisan.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,8 @@ public class PostService {
 
     private PostRepository postRepository;
 
+    private CustomerRepository customerRepository;
+
     public PostService() {}
 
     @Autowired
@@ -25,8 +28,23 @@ public class PostService {
         this.postRepository = postRepository;
     }
 
-    public Post save(PostCommand postCom, Customer customer) {
+    @Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Post createPost(PostCommand postCom) {
+        Customer customer = customerRepository.findCustomerByCustomerId(postCom.getCustomerId());
+        
         Post post = new Post(customer, postCom.getCategId(), new Date(), postCom.getTitle(),
+                postCom.getDesc(), postCom.getImageUrl(), postCom.getWidth(), postCom.getHeight(), postCom.getDepth(), 1, postCom.getPrice());
+        return postRepository.save(post);
+    }
+
+    public Post updatePost(PostCommand postCom) {
+        Customer customer = customerRepository.findCustomerByCustomerId(postCom.getCustomerId());
+
+        Post post = new Post(postCom.getPostId(), customer, postCom.getCategId(), new Date(), postCom.getTitle(),
                 postCom.getDesc(), postCom.getImageUrl(), postCom.getWidth(), postCom.getHeight(), postCom.getDepth(), 1, postCom.getPrice());
         return postRepository.save(post);
     }
